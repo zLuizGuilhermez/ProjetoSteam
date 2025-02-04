fb93faaefc670506642a49126c9778f93b744c44
Luiz Guilherme <Luizerae.s@gmail.com>
2025-02-04 16:53:05 -0300

    s

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,  // ✅ Garante que as rotas funcionem corretamente
-  },
-});
+        historyApiFallback: true,
+        port: process.env.PORT || 3000,
+      }
+  }
+    // ✅ Garante que as rotas funcionem corretamente
+);
